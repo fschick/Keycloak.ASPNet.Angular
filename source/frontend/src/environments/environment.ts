d7a2d37081658c93f3fc89d8dbe7bb1c8a2e857b
Florian Schick <florian.schick@schick-software.de>
2022-12-18 19:39:40 +0100

    Authentication added.

--- source/frontend/src/environments/environment.ts
+++ source/frontend/src/environments/environment.ts
@@ -5,6 +5,9 @@
 export const environment = {
   production: false,
   apiBasePath: `http://${location.hostname}:5050/api`,
+  authServerUrl: `http://${location.hostname}:8080`,
+  realm: 'asp-net-keycloak',
+  clientId: 'api',
 };
 
 /*
