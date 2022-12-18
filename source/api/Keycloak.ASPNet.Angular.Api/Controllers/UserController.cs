6577f1c844d6af14270289f96df6cd2141ebb71a
Florian Schick <florian.schick@schick-software.de>
2022-12-18 19:39:40 +0100

    Authentication added.

--- source/api/Keycloak.ASPNet.Angular.Api/Controllers/UserController.cs
+++ source/api/Keycloak.ASPNet.Angular.Api/Controllers/UserController.cs
@@ -1,8 +1,10 @@
-﻿using Keycloak.ASPNet.Angular.Api.Routing;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Keycloak.ASPNet.Angular.Api.DTOs;
+using Keycloak.ASPNet.Angular.Api.Routing;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Keycloak.ASPNet.Angular.Api.Controllers;
 
@@ -20,6 +22,22 @@ public class UserController : ControllerBase
     /// The product name
     /// </returns>
     [HttpGet]
-    public Task<object> GetCurrentUser(CancellationToken cancellationToken = default)
-        => throw new NotImplementedException();
+    [Authorize]
+    public Task<UserDto> GetCurrentUser(CancellationToken cancellationToken = default)
+    {
+        var user = HttpContext.User;
+        var userFound = user.Identity != null;
+        if (!userFound)
+            return null;
+
+        var userInfo = new UserDto
+        {
+            AuthenticationType = user.Identity.AuthenticationType,
+            IsAuthenticated = user.Identity.IsAuthenticated,
+            Username = user.Identity.Name,
+            Claims = user.Claims.Select(claim => new ClaimDto { Type = claim.Type, Value = claim.Value })
+        };
+
+        return Task.FromResult(userInfo);
+    }
 }
---
+++ source/api/Keycloak.ASPNet.Angular.Api/DTOs/UserDto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Keycloak.ASPNet.Angular.Api.DTOs;
+
+/// <summary>
+/// Information about an user.
+/// </summary>
+public class UserDto
+{
+    /// <summary>
+    /// Gets or initializes the type of the authentication.
+    /// </summary>
+    public string AuthenticationType { get; init; }
+
+    /// <summary>
+    /// Gets or initializes a value indicating whether the user is authenticated.
+    /// </summary>
+    public bool IsAuthenticated { get; init; }
+
+    /// <summary>
+    /// Gets or initializes the user name.
+    /// </summary>
+    public string Username { get; init; }
+
+    /// <summary>
+    /// Access to Authentication 'type' info.
+    /// </summary>
+    public IEnumerable<ClaimDto> Claims { get; init; }
+}
