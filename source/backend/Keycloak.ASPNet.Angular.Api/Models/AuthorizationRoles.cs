6fe1f5085fd6c7f5bebd57b0b27ed11ba4f87022
Florian Schick <florian.schick@schick-software.de>
2022-12-20 19:27:40 +0100

    Authorization via roles added.

---
+++ source/backend/Keycloak.ASPNet.Angular.Api/Models/AuthorizationRoles.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Keycloak.ASPNet.Angular.Api.Models;
+
+/// <summary>
+/// Authorization role names
+/// </summary>
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class AuthorizationRoles
+{
+    /// <summary>
+    /// Manage role.
+    /// </summary>
+    public const string Manage = nameof(Manage);
+}
