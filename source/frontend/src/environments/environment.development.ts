f22a2ff4074c30da4058238f25b1860d8f04f37c
Florian Schick <florian.schick@schick-software.de>
2025-01-24 15:32:50 +0100

    Authorization switched to UMA using roles.

--- source/frontend/src/environments/environment.development.ts
+++ source/frontend/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   apiBasePath: `${location.protocol}//${location.hostname}:5050/api`,
   authority: `<issuer URL>`,
-  clientId: '<client ID>',
+  clientId: '<client ID of login client>',
+  resourceServerId: '<client ID of resouce client>',
 };
